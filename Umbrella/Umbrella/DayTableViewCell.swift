f62b2b8192f70c9b1901e11edbdc72e043f49a12
Demetrio <dem3perez@gmail.com>
2017-09-15 19:54:01 -0400

    Adding DayTableViewCell

---
+++ Umbrella/Umbrella/DayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DayTableViewCell.swift
+//  Umbrella
+//
+//  Created by User on 9/15/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+
+class DayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
