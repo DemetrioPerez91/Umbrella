c202cc3605abf201798d663a4cec454d72cc4d76
Demetrio <dem3perez@gmail.com>
2017-09-16 14:56:53 -0400

    Add extension to get Time
    Added an extension to the Date class to get the current time in hours and minutes

---
+++ Umbrella/Umbrella/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  Umbrella
+//
+//  Created by User on 9/16/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import Foundation
+extension Date
+{
+    func getCurrentTimeHourMinutes() -> String
+    {
+        let date = Date()
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        return "\(hour):\(minutes)"
+    }
+}
