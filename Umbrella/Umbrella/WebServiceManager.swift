74c926c1df0d2ba03c1aba4f04d3ce3219b32c49
Demetrio <dem3perez@gmail.com>
2017-09-16 10:42:55 -0400

    Add WebServiceManager and TestCase

---
+++ Umbrella/Umbrella/WebServiceManager.swift
@@ -0,0 +1,15 @@
+//
+//  WebServiceManager.swift
+//  Umbrella
+//
+//  Created by User on 9/16/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+
+class WebServiceManager: NSObject
+{
+    static let instance = WebServiceManager()
+    override private init(){}
+}
