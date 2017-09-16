c10b2ab3ba8dab142d6798480c8d4793938d3f6b
Demetrio <dem3perez@gmail.com>
2017-09-15 20:02:16 -0400

    Added Forecast Model

---
+++ Umbrella/Umbrella/Forecast.swift
@@ -0,0 +1,24 @@
+//
+//  Forecast.swift
+//  Umbrella
+//
+//  Created by User on 9/15/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import Foundation
+
+
+class Forecast:NSObject
+{
+    let temperature:Int
+    let time:String
+    let weather:String
+    
+    init(temperature:Int, time:String, weather:String)
+    {
+        self.temperature = temperature
+        self.time = time
+        self.weather = weather
+    }
+}
