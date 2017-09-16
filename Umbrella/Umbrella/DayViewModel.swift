d3bbe483e568b0d179f097faa812b29e7f48eb33
Demetrio <dem3perez@gmail.com>
2017-09-16 09:20:12 -0400

    Added Tests for DayViewModel and delete unused  TestCase
    Tested setting hottest forecast and coldest forecast and deleted unused testcase file.

--- Umbrella/Umbrella/DayViewModel.swift
+++ Umbrella/Umbrella/DayViewModel.swift
@@ -16,9 +16,27 @@ class DayViewModel: NSObject
     
     func append(_ forecast:Forecast)
     {
+        let VM = ForecastVM(forecast)
         if forecasts.count == 0
         {
-            forecasts.append(ForecastVM(forecast))
+            
+            forecasts.append(VM)
+            hottest = VM
+            coldest = VM
+            
+        }
+        else
+        {
+            if VM.temperatureInt > (hottest?.temperatureInt)!
+            {
+                hottest = VM
+            }
+            
+            if VM.temperatureInt < (coldest?.temperatureInt)!
+            {
+                coldest = VM
+            }
+            forecasts.append(VM)
         }
         
     }
--- Umbrella/UmbrellaTests/DayTestCase.swift
+++ Umbrella/UmbrellaTests/DayTestCase.swift
@@ -14,12 +14,56 @@ class DayTestCase: XCTestCase {
     var forecast2:Forecast?
     var forecast3:Forecast?
     var forecast4:Forecast?
+    var day:DayViewModel?
     
-    override func setUp() {
-        super.setUp()
-    }
     override func tearDown() {
         super.tearDown()
+    }
+    
+    override func setUp() {
+        forecast1 = Forecast(temperature: 61, time: "lol", weather: "Sunny")
+        forecast2 = Forecast(temperature: 60, time: "lol", weather: "Sunny")
+        forecast3 = Forecast(temperature: 59, time: "lol", weather: "Sunny")
+        forecast4 = Forecast(temperature: 62, time: "lol", weather: "Sunny")
+        
+        day = DayViewModel()
+        appendForecasts()
+        
+        super.setUp()
+    }
+    
+    func appendForecasts()
+    {
+        day?.append(forecast1!)
+        day?.append(forecast2!)
+        day?.append(forecast3!)
+        day?.append(forecast4!)
+    }
+    
+    
+    
+   
+    func testHottest()
+    {
+        guard let fore = day?.hottest?.forecast else {return}
+        guard let fore2 = forecast4 else {return}
+        
+        XCTAssertEqual(fore, fore2)
+        
+    }
+    
+    func testColdest()
+    {
+        guard let fore = day?.hottest?.forecast else {return}
+        guard let fore2 = forecast4 else {return}
+        XCTAssertEqual(fore, fore2)
+        
+    }
+    
+    func testAppendForecast()
+    {
+        
+        XCTAssertEqual(day?.forecasts.count, 4)
     }
     
 
