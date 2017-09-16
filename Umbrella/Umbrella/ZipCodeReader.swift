8fcc8a0e88157dc3f499f4bf20f5abe449f380d7
Demetrio <dem3perez@gmail.com>
2017-09-16 10:13:44 -0400

    Test for valid zip code
    add test cases and refactor reader

--- Umbrella/Umbrella/ZipCodeReader.swift
+++ Umbrella/Umbrella/ZipCodeReader.swift
@@ -13,6 +13,21 @@ class ZipCodeReader: NSObject
     static let instance = ZipCodeReader()
     override private init(){}
     
+    //Check if zipCode is acceptable
+    func isZipCodeValid( _ zipCode:String)->Bool
+    {
+        var result = false
+        if zipCode.characters.count == 5
+        {
+            if Int(zipCode) != nil
+            {
+                DataManager.instance.zipCode = zipCode
+                result = true
+            }
+            
+        }
+        return result
+    }
     
 
 }
--- Umbrella/UmbrellaTests/ZipCodeReaderTestCase.swift
+++ Umbrella/UmbrellaTests/ZipCodeReaderTestCase.swift
@@ -19,5 +19,26 @@ class ZipCodeReaderTestCase: XCTestCase {
         super.tearDown()
     }
     
+    func testInvalid()
+    {
+        let invalidZipCode1 = "AAAaa"
+        let invalidZipCode2 = "1A11A"
+        let invalidZipCode3 = "1A1 A"
+        let invalidZipCode4 = "1A1.A"
+        let invalidZipCode5 = "111.1"
+        let invalidZipCode6 = "111111"
+        XCTAssertFalse(ZipCodeReader.instance.isZipCodeValid(invalidZipCode1))
+        XCTAssertFalse(ZipCodeReader.instance.isZipCodeValid(invalidZipCode2))
+        XCTAssertFalse(ZipCodeReader.instance.isZipCodeValid(invalidZipCode3))
+        XCTAssertFalse(ZipCodeReader.instance.isZipCodeValid(invalidZipCode4))
+        XCTAssertFalse(ZipCodeReader.instance.isZipCodeValid(invalidZipCode5))
+        XCTAssertFalse(ZipCodeReader.instance.isZipCodeValid(invalidZipCode6))
+        
+    }
    
+    func testValid()
+    {
+        let zip = "11111"
+        XCTAssert(ZipCodeReader.instance.isZipCodeValid(zip))
+    }
 }
