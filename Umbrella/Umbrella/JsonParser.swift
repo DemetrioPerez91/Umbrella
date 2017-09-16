a9c88a54657014b6b79c10b33072df923b3799fb
Demetrio <dem3perez@gmail.com>
2017-09-16 13:10:00 -0400

    Added JsonParser and TestCase

---
+++ Umbrella/Umbrella/JsonParser.swift
@@ -0,0 +1,16 @@
+//
+//  JsonParser.swift
+//  Umbrella
+//
+//  Created by User on 9/16/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+
+class JsonParser: NSObject
+{
+    static let  instance = JsonParser()
+    override init(){}
+    
+}
---
+++ Umbrella/UmbrellaTests/JsonParsrTestCase.swift
@@ -0,0 +1,23 @@
+//
+//  JsonParsrTestCase.swift
+//  Umbrella
+//
+//  Created by User on 9/16/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import XCTest
+
+class JsonParsrTestCase: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
