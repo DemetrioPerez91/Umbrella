6e2d7704649dd1e473a7f116f00c7eaeceb4fff5
Demetrio <dem3perez@gmail.com>
2017-09-16 23:31:11 -0400

    Sets Current conditions to CurrnetConditionsView

--- Umbrella/Umbrella/ForecastViewController.swift
+++ Umbrella/Umbrella/ForecastViewController.swift
@@ -11,26 +11,66 @@ import UIKit
 
 class ForecastViewController: UIViewController {
 
+    @IBOutlet weak var currentConditionView: UIView!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var forecastTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        DataManager.instance.refreshConditionsDelegate = self
+        DataManager.instance.refreshtTableDelegate = self
+        forecastTableView.delegate = self
+        forecastTableView.dataSource = self
+        DataManager.instance.refreshConditionsDelegate?.refreshConditions()
+        DataManager.instance.refreshtTableDelegate?.refresh()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension ForecastViewController:UITableViewDelegate{}
+extension ForecastViewController:UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print( DataManager.instance.days.count)
+        return DataManager.instance.days.count
     }
-    */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = forecastTableView.dequeueReusableCell(withIdentifier: "dayCell")
+        cell?.textLabel?.text = "lol"
+        return cell!
+    }
+}
 
+extension ForecastViewController:RefreshTableProtocol
+{
+    func refresh() {
+        DispatchQueue.main.async {
+            self.forecastTableView.reloadData()
+        }
+        
+    }
+}
+
+extension ForecastViewController:RefreshCurrentConditions
+{
+    func refreshConditions()
+    {
+        DispatchQueue.main.async {
+            if let forecast = DataManager.instance.currentConditions
+            {
+                self.stateLabel.text = DataManager.instance.location
+                self.tempLabel.text = forecast.temperatureText
+                self.conditionLabel.text = forecast.forecast.weather
+            }
+        }
+       
+    }
 }
